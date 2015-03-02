ec210e2fed5b4c0b00ccbfad4749baa1d88e4366
Miguel Angel Fernandez <mafesan.nsn@gmail.com>
2015-03-02 12:44:28 +0100

    Tercera modificacion: error si el recurso no existe

--- simple-server-http-2.py
+++ simple-server-http-2.py
@@ -1,20 +1,24 @@
 import SimpleHTTPServer	
-import SocketServer	
+import SocketServer
+from os import curdir, sep, path
   
 PORT = 8000	
 
 class HttpHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):	
     def do_GET(self):	
         print "REQUESTED RESOURCE:", self.path
-        if self.path == "/":
-            self.send_response(200)
-            self.send_header('Content-Type', 'text/html')		
-            self.end_headers()	
-            self.wfile.write("<html><body>ROOT</body></html>")
-        else:
-            self.send_error(404, 'Resource not found: %s'  % self.path)
+        fpath = curdir + sep + self.path
+        try:
+            f = open(fpath)
+            fsize = path.getsize(fpath)
+        except IOError:
+            self. send_error(404, 'File Not Found: %s' % self.path)
+            return
+
+        self.send_response(200)
+        self.end_headers()
+        self.wfile.write(f.read())
   
 httpd =	SocketServer.TCPServer(("", PORT), HttpHandler)  
 print "serving at port", PORT 
 httpd.serve_forever()
- 
