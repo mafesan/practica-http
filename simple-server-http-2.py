92783604161a7e61416fe3f65e2fb3f9a5caaeb7
Miguel Angel Fernandez <mafesan.nsn@gmail.com>
2015-03-02 12:30:46 +0100

    Modificado para mandar un 200 Ok y una respuesta

--- simple-server-http-2.py
+++ simple-server-http-2.py
@@ -4,8 +4,12 @@ import SocketServer
 PORT = 8000	
 
 class HttpHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):	
-  def do_GET(self):	
-  	print "REQUESTED RESOURCE:",self.path
+    def do_GET(self):	
+        print "REQUESTED RESOURCE:", self.path
+        self.send_response(200)
+        self.send_header('Content-Type', 'text/html')		
+        self.end_headers()	
+        self.wfile.write("<html><body>Tiny server response</body></html>")
   
 httpd =	SocketServer.TCPServer(("", PORT), HttpHandler)  
 print "serving at port", PORT 
