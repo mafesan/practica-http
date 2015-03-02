90da362604db3dbdab7e775fd2a0f71d08e61c0b
Miguel Angel Fernandez <mafesan.nsn@gmail.com>
2015-03-02 12:35:56 +0100

    Modificado para mandar 200 para ROOT y 404 para otros recursos

--- simple-server-http-2.py
+++ simple-server-http-2.py
@@ -6,10 +6,13 @@ PORT = 8000
 class HttpHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):	
     def do_GET(self):	
         print "REQUESTED RESOURCE:", self.path
-        self.send_response(200)
-        self.send_header('Content-Type', 'text/html')		
-        self.end_headers()	
-        self.wfile.write("<html><body>Tiny server response</body></html>")
+        if self.path == "/":
+            self.send_response(200)
+            self.send_header('Content-Type', 'text/html')		
+            self.end_headers()	
+            self.wfile.write("<html><body>ROOT</body></html>")
+        else:
+            self.send_error(404, 'Resource not found: %s'  % self.path)
   
 httpd =	SocketServer.TCPServer(("", PORT), HttpHandler)  
 print "serving at port", PORT 
